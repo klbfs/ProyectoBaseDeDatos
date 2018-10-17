79828b7f1f3b12403ff39517bb936b791b6845df
Yocoyani <yocoyaniperez1@gmail.com>
2018-10-16 21:14:31 -0500

    Funciones completo y usuario

---
+++ Usuario.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 28 20:42:43 2018
+
+@author: yocoy
+"""
+from BaseDeDatos import BaseDeDatos 
+
+class Usuario():
+    
+    listaArgumentos = ['nombreDeUsuario','id','password','correo','intereses','ocupacion','descripcion','telefono','tarjetaDeCredito','nivelDeUsuario']
+    _listaObjetos = []
+
+    @classmethod
+    def regresarArgumentos(cls):
+        return cls.listaArgumentos
+
+    @classmethod
+    def regresarObjeto(cls, nombreObjeto):
+        for objeto in cls._listaObjetos:
+            if objeto.nombreDeUsuario == nombreObjeto:
+                return objeto
+
+    @classmethod
+    def eliminarObjeto(cls, objet):
+        for objeto in cls._listaObjetos:
+            if objeto == objet:
+                cls._listaObjetos.remove(objet)
+    
+    @classmethod
+    def editarArgumento(cls, argumento, objeto, dato):
+        if argumento == 'nombreDeUsuario':
+            for obj in cls._listaObjetos:
+                if obj == objeto:
+                    cls._listaObjetos[cls._listaObjetos.index(obj)].nombreDeUsuario = dato
+
+    @staticmethod
+    def crearPrevios():
+        listas = BaseDeDatos.obtenerDatosTotales('Usuario')
+        for lista in listas:
+            EmpresaTecnologia(lista)
+
+    @classmethod
+    def listarObjetos(cls, objeto):
+        cls._listaObjetos.append(objeto)
+
+    def crearBase(self):
+        BaseDeDatos('Usuario',self.listaArgumentos)
+
+    def __init__(self, modo = None):
+        
+        if modo == 1:
+            self.nombreDeUsuario = input('Nombre de Usuario: ')
+            self.id = input('id: ')
+            self.password = input('Password: ')
+            self.correo = input('Correo: ')
+            self.intereses = input('Intereses: ')
+            self.ocupacion = input('Ocupacion: ')
+            self.descripcion = input('Descripcion: ')
+            self.telefono = input('Telefono: ')
+            self.tarjetaDeCredito = input('Tarjeta de credito: ')
+            self.nivelDeUsuario = input('Nivel de usuario: ')
+            self.listarObjetos(self)
+            BaseDeDatos.agregarDatos('Usuario', [self.nombreDeUsuario,self.id,self.password,self.correo,self.intereses,self.ocupacion,self.descripcion,self.telefono,self.tarjetaDeCredito,self.nivelDeUsuario], self.listaArgumentos)
+        elif modo != None:
+            self.nombreDeUsuario = modo[0]
+            self.id = modo[1]
+            self.password = modo[2]
+            self.correo = modo[3]
+            self.intereses = modo[4]
+            self.ocupacion = modo[5]
+            self.descripcion = modo[6]
+            self.telefono = modo[7]
+            self.tarjetaDeCredito = modo[8]
+            self.nivelDeUsuario = modo[9]
+            self.listarObjetos(self)
+        else:
+            self.crearBase()
--- funciones.py
+++ funciones.py
@@ -4,9 +4,25 @@ Created on Sun Sep 23 18:48:17 2018
 
 @author: yocoy
 """
-import SectorAgricola as sA
-import EmpresaTecnologia as eT
 import BaseDeDatos as bd
+import Ubicacion as ub
+import RedesSociales as rs
+import ActividadEconomica as ae
+import Empresa as em
+import Usuario as us
+import Buscador as bus
+import MenuBusqueda as mb
+import CuentaAdministrador as ca
+import InformacionDeLaEmpresa as ide
+import Estadisticas as es
+import Cliente as cl
+import Opiniones as op
+import Fundaciones as fu
+import Capital as cap
+import Departamentos as dep
+import ImagenesYVideo as iyv
+import Instalaciones as ins
+import LideresEmpresariales as le
 
 def  validacionEntera(texto, l1, l2):
     while True:
@@ -22,13 +38,47 @@ def  validacionEntera(texto, l1, l2):
 
 def crearBases():
     
-    nombresClases = ['SectorAgricola','EmpresaTecnologia']
+    nombresClases = ['Ubicacion','RedesSociales','ActividadEconomica','Empresa','Usuario','Buscador','MenuBusqueda','CuentaAdministrador','InformacionDeLaEmpresa','Estadisticas','Cliente','Opiniones','Fundaciones','Capital','Departamentos','ImagenesYVideo','Instalaciones','LideresEmpresariales']
     clases = bd.BaseDeDatos('BaseClases',['nombre'])
     try:
-        bd.BaseDeDatos.comprobarDato('BaseClases', 'nombre', 'EmpresaTecnologia')
+        bd.BaseDeDatos.comprobarDato('BaseClases', 'nombre', 'Ubicacion')
     except:  
         for clase in nombresClases:
            clases.agregarDatos('BaseClases', [clase], ['nombre'])
             
-    tec = eT.EmpresaTecnologia()
-    tec.crearPrevios()
+    ubi = ub.Ubicacion()
+    ubi.crearPrevios()
+    redes = rs.RedesSociales()
+    redes.crearPrevios()
+    act = ae.ActividadEconomica()
+    act.crearPrevios()
+    empr = em.Empresa()
+    empr.crearPrevios()
+    user = us.Usuario()
+    user.crearPrevios()
+    busc = bus.Buscador()
+    busc.crearPrevios()
+    men = mb.MenuBusqueda()
+    mb.crearPrevios()
+    admin = ca.CuentaAdministrador()
+    admin.crearPrevios()
+    info = ide.InformacionDeLaEmpresa()
+    info.crearPrevios()
+    esta = es.Estadisticas()
+    esta.crearPrevios()
+    client = cl.Cliente()
+    client.crearPrevios()
+    opi = op.Opiniones()
+    opi.crearPrevios()
+    fun = fu.Fundaciones()
+    fun.crearPrevios()
+    capi = cap.Capital()
+    capi.crearPrevios()
+    depart = dep.Departamentos()
+    depart.crearPrevios()
+    image = iyv.ImagenesYVideo()
+    image.crearPrevios()
+    instal = ins.Instalaciones()
+    instal.crearPrevios()
+    lid = le.LideresEmpresariales()
+    lid.crearPrevios()
