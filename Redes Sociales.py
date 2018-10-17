0c4af9870cf617aa3ec76b8f5ef8653d8905bb4d
FernandoUNAM <42542746+FernandoUNAM@users.noreply.github.com>
2018-10-17 15:23:13 -0500

    Update Redes Sociales.py

--- Redes Sociales.py
+++ Redes Sociales.py
@@ -11,9 +11,10 @@ class RedesSociales():
         return cls.listaArgumentos
 
     @classmethod
-    def regresarObjeto(cls, nombreObjeto):
+    def regresarObjeto(cls,argumento, nombreObjeto):
         for objeto in cls._listaObjetos:
-            if objeto.Facebook == nombreObjeto:
+            cadena = eval('objeto.'+argumento)
+            if cadena == nombreObjeto:
                 return objeto
 
     @classmethod
