20856c16b4fdc40071406cd50094072be0923f66
FernandoUNAM <42542746+FernandoUNAM@users.noreply.github.com>
2018-10-19 01:29:44 -0500

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -5,23 +5,23 @@ Created on Sun Sep 23 18:48:17 2018
 @author: yocoy
 """
 import BaseDeDatos as bd
-#import Ubicacion as ub
-#import RedesSociales as rs
-#import ActividadEconomica as ae
-#import Empresa as em
+import Ubicacion as ub
+import RedesSociales as rs
+import ActividadEconomica as ae
+import Empresa as em
 import Usuario as us
-#import Buscador as bus
-#import MenuBusqueda as mb
+import Buscador as bus
+import MenuBusqueda as mb
 import CuentaAdministrador as ca
-#import InformacionDeLaEmpresa as ide
+import InformacionDeLaEmpresa as ide
 #import Estadisticas as es
-#import Cliente as cl
-#import Opiniones as op
-#import Fundaciones as fu
-#import Capital as cap
-#import Departamentos as dep
-#import ImagenesYVideo as iyv
-#import Instalaciones as ins
+import Cliente as cl
+import Opiniones as op
+import Fundaciones as fu
+import Capital as cap
+import Departamentos as dep
+import imagenesYvideo as iyv
+import Instalaciones as ins
 #import LideresEmpresariales as le
 
 def  validacionEntera(texto, l1, l2):
@@ -46,39 +46,39 @@ def crearBases():
         for clase in nombresClases:
            clases.agregarDatos('BaseClases', [clase], ['nombre'])
             
-    #ubi = ub.Ubicacion()
-    #ubi.crearPrevios()
-    #redes = rs.RedesSociales()
-    #redes.crearPrevios()
-    #act = ae.ActividadEconomica()
-    #act.crearPrevios()
-    #empr = em.Empresa()
-    #empr.crearPrevios()
+    ubi = ub.Ubicacion()
+    ubi.crearPrevios()
+    redes = rs.RedesSociales()
+    redes.crearPrevios()
+    act = ae.ActividadEconomica()
+    act.crearPrevios()
+    empr = em.Empresa()
+    empr.crearPrevios()
     user = us.Usuario()
     user.crearPrevios()
-    #busc = bus.Buscador()
-#    #busc.crearPrevios()
- #   men = mb.MenuBusqueda()
-  #  mb.crearPrevios()
+    busc = bus.Buscador()
+    busc.crearPrevios()
+    men = mb.MenuBusqueda()
+    men.crearPrevios()
     admin = ca.CuentaAdministrador()
     admin.crearPrevios()
-    #info = ide.InformacionDeLaEmpresa()
-#    info.crearPrevios()
- #   esta = es.Estadisticas()
-  #  esta.crearPrevios()
-   # client = cl.Cliente()
-    #client.crearPrevios()
-#    opi = op.Opiniones()
- #   opi.crearPrevios()
-  #  fun = fu.Fundaciones()
-   # fun.crearPrevios()
-    #capi = cap.Capital()
-#    capi.crearPrevios()
- #   depart = dep.Departamentos()
-  #  depart.crearPrevios()
-   # image = iyv.ImagenesYVideo()
-    #image.crearPrevios()
-#    instal = ins.Instalaciones()
- #   instal.crearPrevios()
-  #  lid = le.LideresEmpresariales()
-   # lid.crearPrevios()
+    info = ide.InformacionDeLaEmpresa()
+    info.crearPrevios()
+    #esta = es.Estadisticas()
+    #esta.crearPrevios()
+    client = cl.Cliente()
+    client.crearPrevios()
+    opi = op.Opiniones()
+    opi.crearPrevios()
+    fun = fu.Fundaciones()
+    fun.crearPrevios()
+    capi = cap.Capital()
+    capi.crearPrevios()
+    depart = dep.Departamentos()
+    depart.crearPrevios()
+    image = iyv.imagenesYvideo()
+    image.crearPrevios()
+    instal = ins.Instalaciones()
+    instal.crearPrevios()
+    #lid = le.LideresEmpresariales()
+    #lid.crearPrevios()
